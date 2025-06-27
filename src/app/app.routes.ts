571a26f0f404cca5cf0a036e2b43b4310a5da778
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-06-27 12:01:11 +0300

    header footer and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Landing } from './pages/landing/landing';
+import { Login } from './pages/login/login';
+import { TimesheetTable } from './components/timesheet-table/timesheet-table';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Landing },
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: TimesheetTable },
+  { path: '**', redirectTo: '' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TimesheetTable } from "./components/timesheet-table/timesheet-table";
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TimesheetTable],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
