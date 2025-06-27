8d6c9e3031d58b8b1303a1f0f7519ed310969c18
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-06-27 14:58:04 +0300

    added fade in animation

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+} from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-landing',
-  imports: [],
+selector: 'app-landing',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './landing.html',
-  styleUrl: './landing.css'
+  styleUrls: ['./landing.css'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('4000ms ease-out', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class Landing {
 
