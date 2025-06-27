ab9ae99d72985ae2d66c9019eda564df0980d367
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-06-27 13:31:14 +0300

    tailwind v3 configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
