7c223e24df641b1fc45b21a63faf2c5d9b674326
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-06-27 11:53:55 +0300

    based pages and components created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TimesheetTable } from "./components/timesheet-table/timesheet-table";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TimesheetTable],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/timesheet-table/timesheet-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timesheet-table',
+  imports: [],
+  templateUrl: './timesheet-table.html',
+  styleUrl: './timesheet-table.css'
+})
+export class TimesheetTable {
+
+}
